dcd41d417c59858a3ab8a453a2b719afbae64d35
Simon Kluibenschädl <sikluibenschaedl123@gmail.com>
2020-08-02 18:58:08 +0200

    Initial commit

---
+++ precache-manifest.5ff011ce18c1fe7f43949edd8ce04fc5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec3ac36ac8af322073ffea52a6798d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f978810116c0f26bc86e",
+    "url": "/static/css/main.c10b2d41.chunk.css"
+  },
+  {
+    "revision": "83e0e7b0314d704584f2",
+    "url": "/static/js/2.c031c420.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c031c420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f978810116c0f26bc86e",
+    "url": "/static/js/main.07e11dc2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "4b713cfeb6d51be8acd2325c8b8b456a",
+    "url": "/static/media/arrow.4b713cfe.svg"
+  },
+  {
+    "revision": "87eb77f34b288275fcf3b8676facc289",
+    "url": "/static/media/calc.87eb77f3.png"
+  },
+  {
+    "revision": "b9ad7beabb172740d7773abaa8791f10",
+    "url": "/static/media/checkout.b9ad7bea.png"
+  },
+  {
+    "revision": "c3b5f6f7d2457b70d8f2df6d755f3ee7",
+    "url": "/static/media/hadu website.c3b5f6f7.png"
+  },
+  {
+    "revision": "eeba9742405dc7fca99102df25f30cf9",
+    "url": "/static/media/horarium.eeba9742.png"
+  },
+  {
+    "revision": "acf747d7b6b3217cf32b8ab4894d34d2",
+    "url": "/static/media/logo.acf747d7.png"
+  },
+  {
+    "revision": "3e6e0524a3e0c3eba795247d99e1fd82",
+    "url": "/static/media/mail.3e6e0524.png"
+  },
+  {
+    "revision": "a57e92b8d91c3ca87ee8470fdb9405da",
+    "url": "/static/media/ostern.a57e92b8.gif"
+  },
+  {
+    "revision": "e682fb5efb14cb76c3001651c23b105f",
+    "url": "/static/media/profile.e682fb5e.png"
+  },
+  {
+    "revision": "301a7b91eb6f1ce80f5a43f3533fabd7",
+    "url": "/static/media/rock.301a7b91.png"
+  }
+]);
